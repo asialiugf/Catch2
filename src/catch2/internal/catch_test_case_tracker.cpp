fe8ce957adacccac1af7a16faf534a6a86f0ace9
Martin Hořeňovský <martin.horenovsky@gmail.com>
2023-02-19 23:53:34 +0100

    Avoid copying NameAndLocationRef when not needed

--- src/catch2/internal/catch_test_case_tracker.cpp
+++ src/catch2/internal/catch_test_case_tracker.cpp
@@ -34,7 +34,7 @@ namespace TestCaseTracking {
         m_runState = NeedsAnotherRun;
     }
 
-    ITracker* ITracker::findChild( NameAndLocationRef nameAndLocation ) {
+    ITracker* ITracker::findChild( NameAndLocationRef const& nameAndLocation ) {
         auto it = std::find_if(
             m_children.begin(),
             m_children.end(),
@@ -173,7 +173,7 @@ namespace TestCaseTracking {
 
     bool SectionTracker::isSectionTracker() const { return true; }
 
-    SectionTracker& SectionTracker::acquire( TrackerContext& ctx, NameAndLocationRef nameAndLocation ) {
+    SectionTracker& SectionTracker::acquire( TrackerContext& ctx, NameAndLocationRef const& nameAndLocation ) {
         SectionTracker* tracker;
 
         ITracker& currentTracker = ctx.currentTracker();
